a46f183d57a1f5b2381db9f22d0b8ec095bd5c5c
gabilgg07 <gabilgg07@gmail.com>
2023-05-30 13:25:04 +0400

    Alert Component alert and prompt functions, Platform Component

--- reactNativeExpoLesson/App.js
+++ reactNativeExpoLesson/App.js
@@ -1,5 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Alert,
+  Platform,
+} from "react-native";
 
 export default function App() {
   const handleTextPress = () => {
@@ -13,30 +21,72 @@ export default function App() {
     console.log("====================================");
   };
 
+  const handlePressAlertBtn = () => {
+    alert("Alert Clicked");
+  };
+
+  const handlePressAlertComponentAlertBtn = () => {
+    // Platform bize hansi device-de oldugumuzu bilmeye komek edir
+    if (Platform.OS === "web") {
+      alert("Alert Component Alert Btn Clicked On Web");
+    } else {
+      Alert.alert(
+        "Title Alert",
+        "Msg: Alert Component Alert Btn Clicked On Other Device",
+        [
+          { text: "Təstiq", onPress: () => console.log("Qəbul edildi") },
+          { text: "İmtina", onPress: () => console.log("Leğv edildi") },
+        ]
+      );
+    }
+  };
+
+  const handlePressAlertComponentPromptBtn = () => {
+    if (Platform.OS === "web") {
+      const text = prompt(
+        "Alert Component Prompt Btn Clicked On Web",
+        "Default Text"
+      );
+      if (text) {
+        console.log("====================================");
+        console.log("text: ", text);
+        console.log("====================================");
+      } else {
+        console.log("Canceled");
+      }
+    } else {
+      // Alert.prompt -> Androidde islemir.
+      Alert.prompt(
+        "Title Prompt",
+        "Msg: Alert Component Prompt Btn Clicked On Other Device",
+        (text) => console.log(text)
+      );
+    }
+  };
+
   return (
-    // View -> div
-    // <View style={styles.container}>
-
-    // Text -> p
-    //   <Text>Hello!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-
     // SafeAreaView - misal iPhone-da gorunus cercivesinde olsun,
     //kameranin altina, ekranin qovsu altina dusmesin.
-
     <SafeAreaView style={styles.container}>
+      {/* View -> div */}
+      <View>
+        {/* Text -> p */}
+        <Text>
+          Example Text {"(like -> p)"} in View {"(like -> div)"}
+        </Text>
+      </View>
+
       {/* alt alta yazilmasi ucun ya asagidaki kimi iki Text componentinden istifade edirik, */}
-      <Text>Hello!</Text>
-      <Text>Hello!</Text>
+      <Text>Hello1.1!</Text>
+      <Text>Hello1.2!</Text>
 
       {/* ya da xususi simvollarla: */}
-      <Text>Hello2! {"\n"}Hello2!</Text>
+      <Text>Hello2.1! {"\n"}Hello2.2!</Text>
 
       {/* numberOfLine atributuna 1 sayi versek ise hemin Text-deki sadece 1ci line da olan gorunecek ios-da, 
       web de ise digerlerini de yanyana getirecek: */}
       <Text numberOfLines={1}>
-        Hello3! {"\n"}Hello3! {"\n"}Hello3!
+        Hello3.1! {"\n"}Hello3.2! {"\n"}Hello3.3!
       </Text>
 
       {/* style icine ancaq ya object sekinde parametrler otururuk,
@@ -65,6 +115,25 @@ export default function App() {
         color="orange"
         onPress={handlePressOrangeBtn}
       />
+      <Text>{"\n"}</Text>
+
+      {/* Alert function */}
+      <Button title="Alert Btn" onPress={handlePressAlertBtn} />
+      <Text>{"\n"}</Text>
+
+      {/* Alert Component alert function */}
+      <Button
+        title="Alert Component alert Btn"
+        onPress={handlePressAlertComponentAlertBtn}
+      />
+      <Text>{"\n"}</Text>
+
+      {/* Alert Component prompt function */}
+      <Button
+        title="Alert Component prompt Btn"
+        onPress={handlePressAlertComponentPromptBtn}
+      />
+      <Text>{"\n"}</Text>
 
       <StatusBar style="auto" />
     </SafeAreaView>
